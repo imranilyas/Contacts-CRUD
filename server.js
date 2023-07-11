83f526289d05bb61c3de53a00a6e9f055bafe517
imranilyas <68575807+imranilyas@users.noreply.github.com>
2023-07-11 15:40:40 -0700

    adding cors such that frontend can connect to backend

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+const cors = require("cors");
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // app.use is considered middleware
+app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
